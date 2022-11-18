77603940939dd57b2f5b69eadb0522a285167ac4
rachel_dooley <rae1821@gmail.com>
2022-11-18 17:40:12 -0500

    Updated projects section

--- js/main.js
+++ js/main.js
@@ -19,5 +19,26 @@ navLinks.forEach(link => {
 //Scroll reveal effects
 ScrollReveal().reveal('about__photo', { delay: 100 });
 ScrollReveal().reveal('.section__title--projects', { delay: 300 });
-ScrollReveal().reveal('.project__content', { delay: 500 });
+ScrollReveal().reveal('.project__content', { delay: 400 });
 
+//Back to top button 
+const backToTopButton = document.querySelector('.back-to-top');
+let isBackToTopRendered = false;
+
+let alterStyles = (isBackToTopRendered) => {
+    backToTopButton.style.visibility = isBackToTopRendered ? "visible" : "hidden";
+    backToTopButton.style.opacity = isBackToTopRendered ? 1 : 0;
+    backToTopButton.style.transform = isBackToTopRendered
+    ? "scale(1)"
+    : "scale(0)";
+};
+
+window.addEventListener("scroll", () => {
+    if (window.scrollY > 700) {
+        isBackToTopRendered = true;
+        alterStyles(isBackToTopRendered);
+    } else {
+        isBackToTopRendered = false;
+        alterStyles(isBackToTopRendered);
+    }
+});
